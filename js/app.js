2411a979d26e8d6d4296929478548629fe651e37
Tobias Kremer <tobias.kremer@comsysto.com>
2015-06-09 15:37:56 +0200

    load attributes form model

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
-angular.module("char-viewer")
-    .controller("MainCtrl")
+angular.module('charViewer', [])
+    .controller('CharCtrl', function($scope) {
+
+        $scope.char = {
+            name: 'Paul',
+            class: 'Paladin',
+            str: 15,
+            dex: 9,
+            con: 16,
+            int: 10,
+            wis: 14,
+            cha: 10
+        };
+
+    });
