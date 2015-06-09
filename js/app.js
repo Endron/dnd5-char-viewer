045fa3ee143cce2196d4863ac58312aeb75475fa
Tobias Kremer <tobias.kremer@comsysto.com>
2015-06-09 16:10:46 +0200

    add filter to calculate attribute modifier

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
-angular.module('charViewer', [])
-    .controller('CharCtrl', function($scope) {
+var app = angular.module('charViewer', []);
+app.controller('CharCtrl', function($scope) {
 
-        $scope.char = {
-            name: 'Paul',
-            class: 'Paladin',
-            str: 15,
-            dex: 9,
-            con: 16,
-            int: 10,
-            wis: 14,
-            cha: 10
-        };
+    $scope.char = {
+        name: 'Paul',
+        class: 'Paladin',
+        str: 15,
+        dex: 8,
+        con: 16,
+        int: 11,
+        wis: 14,
+        cha: 13
+    };
 
-    });
+    $scope.getAttributeModifier = function(attrValue) {
+        return Math.floor((attrValue - 10) / 2);
+    };
+});
+
+app.filter('attributeModifier', function() {
+    return function(attrValue) {
+        var mod = Math.floor((attrValue - 10) / 2);
+        if (mod > 0) {
+            return '+' + mod;
+        } else {
+            return mod;
+        }
+    };
+});
