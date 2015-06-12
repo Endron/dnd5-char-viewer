6506742bedaf09e1969575dafcc0e827afbd2b28
Tobias Kremer <tobias.kremer@comsysto.com>
2015-06-12 10:22:48 +0200

    add JavaDoc

--- src/main/java/charViewer/ApplicationStarter.java
+++ src/main/java/charViewer/ApplicationStarter.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 
+/**
+ * Main class to bootstrap the Spring Context including the embedded webserver used to deliver the website.
+ */
 @SpringBootApplication
 @Controller
 public class ApplicationStarter {
 
+    /**
+     * Starts the application.
+     *
+     * @param args the commandline arguments used
+     */
     public static void main(final String... args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
