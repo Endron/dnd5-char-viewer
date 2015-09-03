775f3bdfb9498ece9fd31e580c2b6d70ae521735
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-03 11:05:07 +0200

    some work on Javadoc to make it more clear.

--- src/main/java/charViewer/ApplicationStarter.java
+++ src/main/java/charViewer/ApplicationStarter.java
@@ -5,17 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 
 /**
- * Main class to bootstrap the Spring Context including the embedded webserver used to deliver the website.
+ * Main class to bootstrap the Spring Context including the embedded webserver used to deliver the website. The
+ * resources will be delivered automatically so no configuration required for now.
  */
 @SpringBootApplication
 @Controller
 public class ApplicationStarter {
 
-    /**
-     * Starts the application.
-     *
-     * @param args the commandline arguments used
-     */
     public static void main(final String... args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
