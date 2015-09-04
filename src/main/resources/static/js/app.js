2ad86adf0b0148536fdeadade09c9167edf8eea4
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-04 13:41:21 +0200

    move attribute names to character sheet controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -22,21 +22,20 @@ angular.module('characterViewer', ['ngMaterial', 'ngRoute'])
         $mdSidenav(menuId).toggle()
     };
 
-    $scope.attributes = [
-        {name: 'Strength', abbr: 'str'},
-        {name: 'Dexterity', abbr: 'dex'},
-        {name: 'Constitution', abbr: 'con'},
-        {name: 'Intelligence', abbr: 'int'},
-        {name: 'Wisdom', abbr: 'wis'},
-        {name: 'Charisma', abbr: 'cha'}
-    ];
-
     $scope.characters = CharacterService.getAll()
 }])
 
 .controller('CharacterSheetCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+        $scope.characterId = $routeParams.characterId;
 
-    $scope.characterId = $routeParams.characterId;
+        $scope.attributes = [
+            {name: 'Strength', abbr: 'str'},
+            {name: 'Dexterity', abbr: 'dex'},
+            {name: 'Constitution', abbr: 'con'},
+            {name: 'Intelligence', abbr: 'int'},
+            {name: 'Wisdom', abbr: 'wis'},
+            {name: 'Charisma', abbr: 'cha'}
+        ];
 }])
 
 .service('CharacterService', [function() {
