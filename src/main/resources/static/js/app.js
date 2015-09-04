65b3e04031a4bf506878a2a89221300da053a3cb
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-04 13:36:17 +0200

    add more data to the character.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -43,8 +43,46 @@ app.service('CharacterService', [function() {
     var service = this
 
     var allCharacters = [
-        {id: 0, name: 'Hans'},
-        {id: 1, name: 'Bob'}
+        {
+            id: 0,
+            name: 'Guruk',
+            classes: [
+                {
+                    name: 'Babarian',
+                    level: 2
+                }
+            ],
+            attributes: {
+                str: 15,
+                dex: 14,
+                con: 13,
+                int: 12,
+                wis: 10,
+                cha: 8
+            }
+        },
+        {
+            id: 1,
+            name: 'Heronimus',
+            classes: [
+                {
+                    name: 'Paladin',
+                    level: 1
+                },
+                {
+                    name: 'Bard',
+                    level: 1
+                }
+            ],
+            attributes: {
+                str: 15,
+                dex: 14,
+                con: 13,
+                int: 12,
+                wis: 10,
+                cha: 8
+            }
+        }
     ];
 
     service.getAll = function() {
