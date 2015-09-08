d90c245de1bd7de6b9874eed86b00570e0ed6788
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-08 16:16:15 +0200

    add skills to skill tab
    just add the name of the skills for now. Bonus will be added later.

--- src/main/resources/static/components/character-sheet/character-sheet-controller.js
+++ src/main/resources/static/components/character-sheet/character-sheet-controller.js
@@ -5,7 +5,29 @@ angular.module('characterViewer.character.sheet', ['ngNewRouter', 'characterView
 function CharacterSheetController(CharacterService, $routeParams) {
     var ctrl = this;
 
+    ctrl.character = {}
     CharacterService.getById($routeParams.characterId).then(function(response) {
         ctrl.character = response.data;
     });
+
+    ctrl.skills = [
+        {name: 'Acrobatics', attribute: 'DEXTERITY'},
+        {name: 'Animal Handling', attribute: 'WISDOM'},
+        {name: 'Arcana', attribute: 'INTELLIGENCE'},
+        {name: 'Athletics', attribute: 'STRENGTH'},
+        {name: 'Deception', attribute: 'CHARISMA'},
+        {name: 'History', attribute: 'INTELLIGENCE'},
+        {name: 'Insight', attribute: 'WISDOM'},
+        {name: 'Intimidation', attribute: 'CHARISMA'},
+        {name: 'Investigation', attribute: 'INTELLIGENCE'},
+        {name: 'Medicine', attribute: 'WISDOM'},
+        {name: 'Nature', attribute: 'INTELLIGENCE'},
+        {name: 'Perception', attribute: 'WISDOM'},
+        {name: 'Performance', attribute: 'CHARISMA'},
+        {name: 'Persuasion', attribute: 'CHARISMA'},
+        {name: 'Religion', attribute: 'INTELLIGENCE'},
+        {name: 'Sleight of Hand', attribute: 'DEXTERITY'},
+        {name: 'Stealth', attribute: 'DEXTERITY'},
+        {name: 'Survival', attribute: 'WISDOM'}
+    ]
 }
