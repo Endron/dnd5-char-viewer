2526c590905eae0edf9e66b22e8809dee6ccda07
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-14 13:05:25 +0200

    add to TODO for some kind of caching.

--- src/main/resources/static/services/character-service.js
+++ src/main/resources/static/services/character-service.js
@@ -1,8 +1,15 @@
 angular.module('charViewer.character.service', [])
 
-    .service('CharacterService', ['$http', CharacterService])
+    .service('CharacterService', ['$http', CharacterService]);
 
 function CharacterService($http) {
+    /*
+     * TODO implement caching
+     * The service should probably implement some kind of caching. We should avoid loading the character with each method
+     * call instead we should have a cache, that stores the characters. This could be as simple as some kind of Map
+     * storing ids and already loaded character.
+     */
+
     var service = this;
 
     service.getAll = function() {
