091f3f781cce6482f6e5408cbdb91165530146a9
Tobias Kremer <tobias.kremer@comsysto.com>
2015-09-15 16:36:16 +0200

    remove unnesessary annotation
    the annotation was nessesary when the application had no real controller
    to enable hosting of static resources. It became obsolete when the
    CharacterController was implemented.

--- src/main/groovy/charViewer/ApplicationStarter.java
+++ src/main/groovy/charViewer/ApplicationStarter.java
@@ -2,14 +2,12 @@ package charViewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 /**
  * Main class to bootstrap the Spring Context including the embedded webserver used to deliver the website. The
  * resources will be delivered automatically so no configuration required for now.
  */
 @SpringBootApplication
-@Controller
 public class ApplicationStarter {
 
     public static void main(final String... args) {
