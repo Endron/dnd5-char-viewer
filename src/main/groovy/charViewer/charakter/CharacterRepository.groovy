4e2e05bd29b8342f778d527a44d1fcae2d06691e
Tobias Kremer <tobias.kremer@comsysto.com>
2016-09-06 19:09:16 +0200

    update core libraries
    - update to Gradle 3.0
    - update to Groovy 2.4.7
    - update to Spring Boot 1.4.0

--- src/main/groovy/charViewer/charakter/CharacterRepository.groovy
+++ src/main/groovy/charViewer/charakter/CharacterRepository.groovy
@@ -2,59 +2,58 @@ package charViewer.charakter
 
 import org.springframework.stereotype.Service
 
+import static charViewer.charakter.Attribute.*
+
 @Service
 public class CharacterRepository {
 
-    private final List<Character> characters
+  private final List<Character> characters
 
-    public CharacterRepository() {
-        characters = []
-
-        final Character heronimus = new Character(
-                name: 'Heronimus',
-                classes: [
-                        new CharacterClass(name: 'Paladin', level: 1),
-                        new CharacterClass(name: 'Worlock', level: 1)
-                ],
-                attributes: [
-                        STRENGTH: 12,
-                        DEXTERITY: 16,
-                        CONSTITUTION: 12,
-                        INTELLIGENCE: 10,
-                        WISDOM: 10,
-                        CHARISMA: 16
-                ]
+  public CharacterRepository() {
+    characters = [
+        new Character(
+            name: 'Heronimus',
+            classes: [
+                new CharacterClass(name: 'Paladin', level: 1),
+                new CharacterClass(name: 'Warlock', level: 1)
+            ],
+            attributes: [
+                (STRENGTH)    : 12,
+                (DEXTERITY)   : 16,
+                (CONSTITUTION): 12,
+                (INTELLIGENCE): 10,
+                (WISDOM)      : 10,
+                (CHARISMA)    : 16
+            ]
+        ),
+        new Character(
+            name: 'Guruk',
+            classes: [
+                new CharacterClass(name: 'Babarian', level: 2)
+            ],
+            attributes: [
+                (STRENGTH)    : 15,
+                (DEXTERITY)   : 10,
+                (CONSTITUTION): 15,
+                (INTELLIGENCE): 10,
+                (WISDOM)      : 8,
+                (CHARISMA)    : 12
+            ],
+            skillProficiency: [
+                'Acrobatics'
+            ]
         )
-        characters.add(heronimus)
+    ]
+  }
 
-        final Character guruk = new Character(
-                name: 'Guruk',
-                classes: [
-                        new CharacterClass(name: 'Babarian', level: 2)
-                ],
-                attributes: [
-                        STRENGTH: 15,
-                        DEXTERITY: 10,
-                        CONSTITUTION: 15,
-                        INTELLIGENCE: 10,
-                        WISDOM: 8,
-                        CHARISMA: 12
-                ],
-                skillProficiency: [
-                        'Acrobatics'
-                ]
-        )
-        characters.add(guruk)
-    }
+  public List<Character> findAll() {
+    return characters
+  }
 
-    public List<Character> findAll() {
-        return characters
-    }
-
-    public Optional<Character> findById(String id) {
-        return characters
-                .stream()
-                .filter { it.id == id }
-                .findFirst()
-    }
+  public Optional<Character> findById(String id) {
+    return characters
+        .stream()
+        .filter { it.id == id }
+        .findFirst()
+  }
 }
